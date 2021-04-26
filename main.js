76b65669ec150450c6173c7c5d6dc8b9c3d9f7a8
Ronin Chasan <rcc263@cornell.edu>
2021-04-26 16:49:40 -0400

    implement switch case

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
 // take in arguments from command line
-args = process.argv;
+let args = process.argv;
 
 //trim files from array, rename
-nums = args.slice(2);
+let nums = args.slice(2);
 
+let words = [];
+var currentString = "";
+nums.forEach(num => {
+    currentString = "";
+
+    for (i=0;i<num.length; i++){
+        char = num[i];
+
+        switch (char){
+            case '0':
+                currentString += 'Zero';
+                break;
+            case '1':
+                currentString += 'One';
+                break;
+            case '2':
+                currentString += 'Two';
+                break;
+            case '3':
+                currentString += 'Three';
+                break;
+            case '4':
+                currentString += 'Four';
+                break;
+            case '5':
+                currentString += 'Five';
+                break;
+            case '6':
+                currentString += 'Six';
+                break;
+            case '7':
+                currentString += 'Seven';
+                break;
+            case '8':
+                currentString += 'Eight';
+                break;
+            case '9':
+                currentString += 'Nine';
+                break;
+            default:
+                break;
+        }
+    }
+})
